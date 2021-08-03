9ee47852fb8f8540a1a343112fe4d0ea7f96ee9b
Korjakos <nurumov.ol@mail.ru>
2021-08-03 08:09:00 +0600

    ex

---
+++ ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите рост (см.): ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите вес (кг.): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            //Рассчитать и вывести индекс массы тела (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.
+            var h = height / 100;
+            var i = weight / (h * h);
+            Console.WriteLine("Индекс массы тела =" + i);
+        }
+    }
+}
