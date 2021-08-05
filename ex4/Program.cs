837909b54d3914e4a682fd0036e5dc257ce5eab1
Korjakos <nurumov.ol@mail.ru>
2021-08-05 23:43:47 +0600

    ДЗ №2

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -11,11 +11,11 @@ namespace ex4
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите второе целое число y: "); 
             int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("число х= {0} число у= {1}", num1, num2);
+            Console.WriteLine("число х= {0}  число у= {1}", num1, num2);
             int x = num1;                   //присваиваем num1 к х
             num1 = num2;                    //присваиваем num1 к num2
             num2 = x;                       // присваиваем х к num2
-            Console.WriteLine("число х= {0} число у= {1}", num1, num2);
+            Console.WriteLine("число х= {0}  число у= {1}", num1, num2);
         }
     }
 }
